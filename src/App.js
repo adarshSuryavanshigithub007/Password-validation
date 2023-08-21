b08c47b00c855735aa8afe7ee61f224e45dd3696
Your Name <you@example.com>
2023-08-21 21:12:19 +0530

    validation install toastify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+
+
 import validator from "validator";
 function App() {
-  const[errorMessage,setErrorMessage]=useState('')
-  const validation=(value)=>{
-if(validator.isStrongPassword(value,{minLength:8,minLowercase:1,minUppercase:1,minNumbers:1,minSymbols:1})){
-  setErrorMessage(`Is Strong Password`)
+  const [password, setPassword] = useState('')
+  const validation = (value) => {
+    if (validator.isStrongPassword(password, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })) {
+      toast.success(`Is Strong Password`)
 
-}else{
-setErrorMessage(`Is Not Strong Password`)
-}
+    } else {
+      toast.error(`Is Not Strong Password`)
+    }
+  }
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
   }
   return (
-    <div >
-      <h2>Check Password Strength in react</h2>
-      <div style={{display:'flex',justifyContent:'center'}}>
-      <span>Enter Password</span><input type="text" onChange={(e)=>validation(e.target.value)}/>
-      <br/>
-      {errorMessage===''?null:<span style={{fontWeight:'bold',color:'red'}}>{errorMessage}</span>}
+    <div  >
+      <ToastContainer style={{ position: toast.POSITION.BOTTOM_RIGHT }} />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h2>Check Password Strength in react</h2>
+
       </div>
-     
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <span>Enter Password</span><input type="text" onChange={handleChangePassword} />
+
+        <button onClick={validation}>submit</button>
+
+
+      </div>
+
     </div>
   );
 }
